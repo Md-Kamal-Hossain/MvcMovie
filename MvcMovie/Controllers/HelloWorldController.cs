da998f010ec52e2eba4d732bd0e88f0b1400ac06
Kamal Hossain <k.hossain@rrive.com>
2023-07-31 21:14:06 +0200

    Add project files.

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index(string name, int numtimes = 1)
+        {
+            //return View();
+            //return HttpUtility.HtmlEncode("Hello"+name+",Numtimes is"+numtimes);
+            ViewBag.Message = name;
+            ViewBag.NumTimes = numtimes;
+            return View();
+        }
+        public ActionResult Welcome(string name, int numtimes = 1)
+        {
+            ////return HttpUtility.HtmlEncode("Hello"+name+",Numtimes is"+numtimes);
+            ViewBag.Message = name;
+            ViewBag.NumTimes = numtimes;
+            return View();
+        }
+    }
+}
---
+++ MvcMovie/Models/Movie.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcMovie.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime ReleaseDate { get; set; }
+
+        [Range(1, 100)]
+        [DataType(DataType.Currency)]
+        public decimal Price { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
+        public string Genre { get; set; }
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [StringLength(5)]
+        public string Rating { get; set; }
+    }
+    public class MovieDBCOntext:DbContext
+    {
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
